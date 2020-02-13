ec611219708de84f6648eb9666418ebebbfdb5b5
Li <1661214933@qq.com>
2020-02-13 20:03:08 +0800

    StringAndHash

--- src/main/java/com/li/RedisTemplate/Main.java
+++ src/main/java/com/li/RedisTemplate/Main.java
@@ -2,6 +2,8 @@ package com.li.RedisTemplate;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 
@@ -14,15 +16,21 @@ import org.springframework.data.redis.core.SessionCallback;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("RedisTemplate.xml");
-        RedisTemplate redisTemplate= (RedisTemplate) context.getBean("redisTemplate");
-        Role role=new Role();
-        role.setId(1L);
-        role.setRoleName("role_name_1");
-        redisTemplate.opsForValue().set("role_1", role);
-        Role role_1 = (Role) redisTemplate.opsForValue().get("role_1");
+        RedisTemplate redisTemplate = context.getBean(RedisTemplate.class);
+        redisTemplate.opsForValue().set("number", "5");
 
-        System.out.println(role_1);
+        Long increment = redisTemplate.opsForValue().increment("number", 5);
 
+        System.out.println("number:"+increment);
 
+        Long number = redisTemplate.getConnectionFactory().getConnection().decr(redisTemplate.getKeySerializer().serialize("number"));
+        System.out.println("redisTemplate.getConnectionFactory.getConnection.decr(redisTemplate.getKeySerializer().serialize("+"nubmer))"+"\n"+number);
+
+        Long decrBy = redisTemplate.getConnectionFactory().getConnection().decrBy(redisTemplate.getKeySerializer().serialize("number"), 3);
+        System.out.println(decrBy);
+
+        Double increment1 = redisTemplate.opsForValue().increment("number", 3.3);
+
+        System.out.println(increment1);
     }
 }
